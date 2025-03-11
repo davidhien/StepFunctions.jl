ea9f229d91738ea251c4e0ec907c6def1974fcf2
davidhien <david.hien@outlook.de>
2025-03-11 11:31:36 +0100

    initial commit with step function iterator

--- src/StepFunctions.jl
+++ src/StepFunctions.jl
@@ -1,5 +1,71 @@
 module StepFunctions
+    import Base: iterate, isdone, length
+    import Base: (+), (-), (*), (/), (^)
 
-# Write your package code here.
+    """
+        struct StepFunction{X<:Real,Y}
+    
+    A step function is a piecewise constant function.
+    It is represented by vectors `xs` and `ys` as follows:
+    ``f(t) = \\begin{cases} ys[i+1] & \\text{if } xs[i] \\leq t < xs[i+1] \\\\ ys[1]   & \\text{if } t < xs[1] \\\\ ys[end] & \\text{if } t \\geq xs[end] \\end{cases}``
 
+    In particular, we must have `length(xs)+1 == length(ys)`. Furthermore, `xs` must be sorted and must not contain `Inf`.
+    """
+    struct StepFunction{X<:Real,Y}
+        xs::Vector{X}
+        ys::Vector{Y}
+
+        function StepFunction(xs,ys)
+            if length(ys) != length(xs)+1
+                throw(ArgumentError("length(xs)+1 must be equal to length(ys)"))
+            elseif !issorted(xs)
+                throw(ArgumentError("xs must be sorted"))
+            elseif xs[end] == Inf
+                throw(ArgumentError("xs must not contain Inf"))
+            else
+                new{eltype(xs),eltype(ys)}(xs,ys)
+            end
+        end
+    end
+
+    struct StepFunctionIterator{T}
+        fcts::T
+    end
+
+    function length(iter::StepFunctionIterator{T}) where T
+        return 1 + sum(f-> length(f.xs),iter.fcts)
+    end
+
+    function iterate(iter::StepFunctionIterator{T}) where {T}
+        return (-Inf,map(f->f.ys[1],iter.fcts)), zeros(Int,length(iter.fcts))
+    end
+
+    function iterate(it::StepFunctionIterator{T}, state) where {T}
+        n = length(it.fcts)
+        minval, min_ind = findmin(1:n) do i
+            succ_i = state[i]+1
+            xs = it.fcts[i].xs
+            if succ_i <= length(xs)
+                return xs[succ_i]
+            else
+                return Inf
+            end
+        end
+        if minval == Inf
+            return nothing
+        end
+        state[min_ind] += 1
+        ys_new = map(t-> t[1].ys[t[2]+1], zip(it.fcts,state))
+
+        return (minval,ys_new), state
+    end
+
+    function isdone(iter::StepFunctionIterator{T}, state) where {T}
+        return state === nothing || all(zip(iter.fcts,state)) do t
+            f,k = t
+            return length(f.xs) == k
+        end
+    end
+
+    export StepFunction, StepFunctionIterator
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,43 @@ using StepFunctions
 using Test
 
 @testset "StepFunctions.jl" begin
-    # Write your tests here.
+    @testset "StepFunction constructor" begin
+        xs = [1,2,3,4]
+        ys = [0,1,2,3,4]
+        f = StepFunction(xs,ys)
+        @test f.xs == xs && f.ys == ys
+
+        # test incompatible lengths
+        xs = [1,2,3,4]
+        ys = [0,1,2,3]
+        @test_throws ArgumentError StepFunction(xs,ys)
+
+        # test unsorted xs
+        xs = [1,3,2,4]
+        ys = [0,1,2,3,4]
+        @test_throws ArgumentError StepFunction(xs,ys)
+
+        # test Inf in xs
+        xs = [1,2,3,Inf]
+        ys = [0,1,2,3,4]
+        @test_throws ArgumentError StepFunction(xs,ys)
+    end
+
+    @testset "StepFunctionIterator" begin
+        fs = map(i->StepFunction([i],[i,i+1]),1:4)
+        it = StepFunctionIterator(fs)
+        @test length(it) == 5
+
+        state = zeros(Int,4)
+        @test iterate(it) == ((-Inf,[1,2,3,4]), state)
+        t,state = iterate(it,state)
+        @test (t,state) == ((1,[2,2,3,4]), [1,0,0,0])
+        t,state = iterate(it,state)
+        @test (t,state) == ((2,[2,3,3,4]), [1,1,0,0])
+        t,state = iterate(it,state)
+        @test (t,state) == ((3,[2,3,4,4]), [1,1,1,0])
+        t,state = iterate(it,state)
+        @test (t,state) == ((4,[2,3,4,5]), [1,1,1,1])
+        @test nothing === iterate(it,state)
+    end
 end
